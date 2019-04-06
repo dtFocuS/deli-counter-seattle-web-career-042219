34570915a489f9be34ac1d1ac3c9ffe766ab24ea
Danny Tseng <dtfocus1025@gmail.com>
2019-04-06 23:29:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,10 @@
 # Write your code here.
 
 def line(current_line)
-  
+  if current_line.size != 0 
+    puts "The line is curently: "
+    current_line.each_with_index {|name, i| print "#{i}. #{name} "
+      
+    end
+  end
 end
