d27d45d1a417c5adbd5326c8be84aa6173b08de3
Danny Tseng <dtfocus1025@gmail.com>
2019-04-06 23:31:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 
 def line(current_line)
   if current_line.size != 0 
-    puts "The line is curently: "
+    puts "The line is currently: "
     current_line.each_with_index {|name, i| print "#{i}. #{name} "}
   else
     puts "The line is currently empty."
