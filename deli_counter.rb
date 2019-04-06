b1dda656113d1d4dafabf686f08b2b85b882fc29
Danny Tseng <dtfocus1025@gmail.com>
2019-04-06 23:30:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@
 def line(current_line)
   if current_line.size != 0 
     puts "The line is curently: "
-    current_line.each_with_index {|name, i| print "#{i}. #{name} "
-      
-    end
+    current_line.each_with_index {|name, i| print "#{i}. #{name} "}
+  else
+    puts "The line is cur"
   end
 end
