d5cf4c401c71acd11490d4af1463cea40489c893
Danny Tseng <dtfocus1025@gmail.com>
2019-04-07 00:09:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,12 +2,11 @@
 
 def line(current_line)
   str = ""
-  if current_line.size != 0 
-    print "The line is currently:"
-    current_line.each_with_index {|name, i| print " #{i + 1}. #{name}"}
-    
-  else
+  if current_line.size == 0 
     puts "The line is currently empty."
+  else
+    str = "The line is currently:"
+    current_line.each_with_index {|name, i| str + " #{i + 1}. #{name}"}
   end
 end
 
